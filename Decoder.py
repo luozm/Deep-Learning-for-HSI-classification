107cc9339de6638a8c32dc0b65569e504dcf74e8
Zhimeng Luo <zhimengluo@foxmail.com>
2017-04-10 14:04:23 +0800

    Update Decoder.py

--- Decoder.py
+++ Decoder.py
@@ -10,7 +10,7 @@ import os
 DATA_PATH = Utils.data_path
 input_image = scio.loadmat(os.path.join(DATA_PATH, Utils.data_file + '.mat'))[Utils.data_name]
 output_image = scio.loadmat(os.path.join(DATA_PATH, Utils.data_file + '_gt.mat'))[Utils.data_name + '_gt']
-model_name = os.path.join(Utils.model_path, '2D-CNN/2D-CNN-' + str(Utils.patch_size) + '.ckpt-250000')
+model_name = os.path.join(Utils.model_path, '3D-CNN/3D-CNN-' + str(Utils.patch_size) + '.ckpt-50000')
 
 height = output_image.shape[0]
 width = output_image.shape[1]
@@ -24,16 +24,6 @@ PATCH_IDX = int((PATCH_SIZE - 1) / 2)
 input_image = input_image.astype(float)
 input_image -= np.min(input_image)
 input_image /= np.max(input_image)
-
-# extend the margin of the origin image
-
-input_mirror = np.zeros(((height + PATCH_SIZE - 1), (width + PATCH_SIZE - 1), BAND))
-input_mirror[PATCH_IDX:(height+PATCH_IDX), PATCH_IDX:(width+PATCH_IDX), :] = input_image[:]
-input_mirror[PATCH_IDX:(height+PATCH_IDX), :PATCH_IDX, :] = input_image[:, PATCH_IDX-1::-1, :]
-input_mirror[PATCH_IDX:(height+PATCH_IDX), (width+PATCH_IDX):, :] = input_image[:, :(width-PATCH_IDX-1):-1, :]
-input_mirror[:PATCH_IDX, :, :] = input_mirror[(PATCH_IDX*2-1):(PATCH_IDX-1):-1, :, :]
-input_mirror[(height+PATCH_IDX):, :, :] = input_mirror[(height+PATCH_IDX-1):(height-1):-1, :, :]
-input_mirror_transposed = np.transpose(input_mirror, (2, 0, 1))
 
 
 def mirror(labels):
@@ -60,26 +50,31 @@ def patch_margin(height_index, width_index):
     -- mean_normalized_patch: mean normalized patch of size (BAND, PATCH_SIZE, PATCH_SIZE)
     whose top left corner is at (height_index, width_index)
     """
-    
+    input_mirror = np.zeros(((height + PATCH_SIZE - 1), (width + PATCH_SIZE - 1), BAND))
+    # extend the margin of the origin image
+    input_mirror[PATCH_IDX:(height+PATCH_IDX), PATCH_IDX:(width+PATCH_IDX), :] = input_image[:]
+    input_mirror[PATCH_IDX:(height+PATCH_IDX), :PATCH_IDX, :] = input_image[:, PATCH_IDX-1::-1, :]
+    input_mirror[PATCH_IDX:(height+PATCH_IDX), (width+PATCH_IDX):, :] = input_image[:, :(width-PATCH_IDX-1):-1, :]
+    input_mirror[:PATCH_IDX, :, :] = input_mirror[(PATCH_IDX*2-1):(PATCH_IDX-1):-1, :, :]
+    input_mirror[(height+PATCH_IDX):, :, :] = input_mirror[(height+PATCH_IDX-1):(height-1):-1, :, :]
+    transpose_array = np.transpose(input_mirror, (2, 0, 1))
+
     height_slice = slice(height_index, height_index + PATCH_SIZE)
     width_slice = slice(width_index, width_index + PATCH_SIZE)
-    patches = input_mirror_transposed[:, height_slice, width_slice]
+    patches = transpose_array[:, height_slice, width_slice]
     mean_normalized_patch = []
     for i in range(patches.shape[0]):
         mean_normalized_patch.append(patches[i] - MEAN_ARRAY[i])
-    patch = np.array(mean_normalized_patch)
-    patch = np.transpose(patch, (1, 2, 0))
-    patch = patch.reshape(1, patch.shape[0], patch.shape[1], patch.shape[2])
-    return patch
+
+    return np.array(mean_normalized_patch)
 
 
 def decoder():
     with tf.Graph().as_default():
         x_placeholder = tf.placeholder(tf.float32, shape=(None, Utils.patch_size, Utils.patch_size, Utils.bands))
 #        y_placeholder = tf.placeholder(tf.int32, shape=None)
-        is_training = tf.placeholder(tf.bool, name='is-training')  # used for dropout
-        
-        logits = models.cnn_2d(x_placeholder, is_training)
+
+        logits = models.cnn_2d(x_placeholder, False)
         softmax = tf.nn.softmax(logits)
 #        eval_all = models.evaluation(logits, y_placeholder)
 
@@ -88,26 +83,57 @@ def decoder():
         with tf.Session() as sess:
 
             saver.restore(sess, model_name)
+
             outputs = np.zeros((height, width))
+            predicted = [[0 for _ in range(width)] for _ in range(height)]
 
             for i in range(height):
                 for j in range(width):
                     target = int(output_image[i, j])
                     if target != 0:
                         image_patch = patch_margin(i, j)
-                        prediction = sess.run(softmax, feed_dict={x_placeholder: image_patch, is_training: False})
-                        outputs[i, j] = np.argmax(prediction) + 1
-                print ('Now progress: %.2f ' % (float(i) / height * 100) + '%')
-    return outputs
+                        # print image_patch
+                        prediction = sess.run(softmax, feed_dict={x_placeholder: image_patch})
+                        # print prediction
+                        temp = np.argmax(prediction) + 1
+                        # print temp1
+                        outputs[int(i + PATCH_SIZE / 2)][int(j + PATCH_SIZE / 2)] = temp
+                        predicted[int(i + PATCH_SIZE / 2)][int(j + PATCH_SIZE / 2)] = prediction
+                print ('Now progress: %.2f ' % (float(i) / (height - PATCH_SIZE) * 100) + '%')
+
+    return outputs, predicted
+
+def cr():
+    """
+    Collaborative Representation.
+    An alternative classifier compare with Softmax
+    
+    Proj_M = inv(train_data'*train_data+kappa*eye(size(train_data,2)))*train_data';
+    
+    coef         =  Proj_M*test_data;
+    for ci = 1:max(Dlabels)
+        coef_c   =  coef(Dlabels==ci);
+        Dc       =  D(:,Dlabels==ci);
+        error(ci) = norm(y-Dc*coef_c,2)^2/sum(coef_c.*coef_c);
+    end
+
+    index      =  find(error==min(error));
+    id         =  index(1);
+    
+    """
+
+    
 
 
+	
+	
 # Calculate the mean of each channel for normalization
 MEAN_ARRAY = np.ndarray(shape=(Utils.bands,), dtype=float)
 for i in range(Utils.bands):
     MEAN_ARRAY[i] = np.mean(input_image[:, :, i])
 
 # Prediction & show image
-predicted_image = decoder()
+predicted_image, predicted_results = decoder()
 
 # Save result
 ground_truth = spectral.imshow(classes=output_image, figsize=(5, 5))
